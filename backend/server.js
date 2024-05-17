9c85b8005d3f449df586ff2b7bf55c4ac2a3aab9
MXMaddux <maxmaddux@gmail.com>
2024-05-17 04:59:17 -0700

    Start errorMiddleware

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+// import cookieParser from "cookie-parser";
+dotenv.config();
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import {
+  notFound,
+  errorHandler,
+  checkObjectId,
+} from "./middleware/errorMiddleware.js";
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+app.use(checkObjectId);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
